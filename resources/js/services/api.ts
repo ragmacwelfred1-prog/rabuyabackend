ef1d52a44b9a2fb9f0761a730242e0d52075e3b3
WakimuraCode <ragmacwelfred1@gmail.com>
2026-09-18 23:14:59 +0800

    Update api.ts

--- resources/js/services/api.ts
+++ resources/js/services/api.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8000/api",
+    baseURL: "rabuyabackend-production.up.railway.app/api",
     headers: {
         "Content-Type": "application/json",
         Accept: "application/json",
